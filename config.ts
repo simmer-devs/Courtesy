221e11de8df14df4d461d77eb4f0b5b880bba9cf
Simmer <55367994+simmer-devs@users.noreply.github.com>
2019-10-26 12:44:48 -0600

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,18 @@
+export let config = {
+    "token": "NjMwODAyNjQ4MjA3MTk2MTYx.XZwRag._A_O9xZPheoE85UmcT15HMA5po8",
+    "commands": [
+      "test",
+      "emit",
+      "settings"
+    ],
+    "defaultSettings": {
+      prefix: '!',
+      welcomeChannel: 'welcome',
+      welcomeMsg: 'Welcome {{user}} to {{guild}}!',
+      modRole: 'Moderator',
+      adminRole: 'Administrator'
+    },
+    "badWords": [
+      'nigger'
+    ] 
+}
