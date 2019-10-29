48184468e8259d491ab9a237af312d1a3dc874a0
Simmer <kelly.matthew1218@gmail.com>
2019-10-28 22:19:29 -0600

    included custom prefix, add/remove censored words from database

--- Commands/settings.ts
+++ Commands/settings.ts
@@ -1,6 +1,6 @@
 import * as Discord from "discord.js";
 import {IBotCommand} from "../api";
-import { updateGuild } from "../Handlers/dbFunctions";
+import { updateGuild, deleteBadword } from "../Handlers/dbFunctions";
 
 export default class settings implements IBotCommand{
     
@@ -26,12 +26,36 @@ export default class settings implements IBotCommand{
                         return;
                     }
                     try{
-                        await updateGuild(message.guild, { prefix: settingUpdate})
+                        await updateGuild(message.guild, { prefix: settingUpdate })
                         message.channel.send(`Guild Prefix has been updated to: \`${settingUpdate}\``)
                     }catch(err){
                         message.channel.send(`An error occured: ${err.message}`)
                     }
                     break;
+                }
+                case 'censor': {
+                    if(!settingUpdate){
+                        message.channel.send(`Current censored words: ||${settings.badWords}||`)
+                        return;
+                    }
+                    try{
+                        await updateGuild(message.guild, { badWords: [...settings.badWords, settingUpdate]})
+                    }catch(err){
+                        console.log(err)
+                    }
+                    break;
+                }
+                case 'censorRemove': {
+                    if(!settingUpdate){
+                        message.channel.send(`Please include a currently censored word to remove. Use '${settings.prefix}settings censor' to see a list of currently censored words.`)
+                        return;
+                    }
+                    try{
+                        await deleteBadword(settingUpdate, message.guild, settings)
+                        return;
+                    }catch(err){
+                        console.log(err)
+                    }
                 }
                 default: {
                     message.channel.send('Please provide a setting to view/update')
