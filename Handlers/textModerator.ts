fbd693f614c1b8e6d566a6cddb7477925cf1a3bc
Simmer <kelly.matthew1218@gmail.com>
2019-11-03 10:38:38 -0700

    text moderator update - emoji

--- Handlers/textModerator.ts
+++ Handlers/textModerator.ts
@@ -1,18 +1,19 @@
 import * as Discord from 'discord.js'
 const { client } = require("../index")
 
-export const handleText = (message: Discord.Message, settings: any) => {
+export const handleText = (message: Discord.Message, guildSettings: any) => {
     let modChannel = message.guild.channels.find(ch => ch.name === 'courtesy-log') as Discord.TextChannel
     
-    for(const key in settings.badWords){
-        //console.log(`${key}: ${settings.badWords[key]}`)
-        if(message.content.toLowerCase().includes(settings.badWords[key])){
-            message.delete()
+    //if(message.author.id === message.guild.owner.id){ return }
+
+    for(const key in guildSettings.badWords){
+        if(message.content.toLowerCase().includes(guildSettings.badWords[key])){
+            message.delete().catch(err => console.log(err))
             let modEmbed = new Discord.RichEmbed()
                     .setColor([206, 145, 190])
                     .setTitle("__Courtesy: Message Moderator Report__")
                     .setAuthor('Courtesy', client.user.avatarURL)
-                    .setDescription(`**Violation by <@${message.author.id}>**\nOriginating Channel: ${message.channel}\nModerated Message: ${message.content}`)
+                    .setDescription(`**Violation by <@${message.author.id}>**\n<:bullet:640379888607428632> Originating Channel: ${message.channel}\n<:bullet:640379888607428632> Moderated Message: ${message.content}`)
                     .setTimestamp()
                 modChannel.send(modEmbed)
                 message.author.send(modEmbed)
