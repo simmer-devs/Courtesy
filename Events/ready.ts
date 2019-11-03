e2075b6f116dd973d024c96b9b58d2503b0c0946
Simmer <kelly.matthew1218@gmail.com>
2019-11-03 10:31:41 -0700

    set activity for user cache

--- Events/ready.ts
+++ Events/ready.ts
@@ -1,6 +1,17 @@
+import { updateSpam } from "../Handlers/memberFunctions"
+const mongoose = require('mongoose')
 const { client } = require("../index")
 
-client.on("ready", () => {
+
+client.on("ready", async () => {
     console.log(":courtesy:")
-    client.user.setActivity("for GAMERS", {type: "WATCHING"});
+    client.user.setActivity(`${client.users.size} Users`, {type: 'WATCHING'})
+    setInterval(() => {
+        client.user.setActivity(`${client.users.size} Users`, {type: 'WATCHING'})
+    },3.6e+6)
+    mongoose.set('useFindAndModify', false)
+    setInterval(async () => {
+        await updateSpam(client)
+    }, 10000)
+    
 })
