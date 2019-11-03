827c4efb5dde9d1102482c56892e8a6fbf53f432
Simmer <kelly.matthew1218@gmail.com>
2019-11-03 10:29:23 -0700

    new guild channel creation fixes

--- Events/guildCreate.ts
+++ Events/guildCreate.ts
@@ -3,6 +3,9 @@ const { client } = require("../index")
 const { createGuild } = require('../Handlers/dbFunctions')
 
 client.on('guildCreate', async (guild: Discord.Guild) => {
+    let everyoneRole = guild.roles.get(`${guild.id}`) as Discord.Role
+    
+    //update newGuildEmbed to look better
     let newGuildEmbed = new Discord.RichEmbed()
         .setColor([206, 145, 190])
         .setTitle('Thanks for the invite!')
@@ -23,5 +26,23 @@ client.on('guildCreate', async (guild: Discord.Guild) => {
         console.log(err)
     }
 
-
+    await guild.createChannel('Courtesy Log', {type: 'text'}).then(channel => {
+        channel.overwritePermissions(everyoneRole, {
+            'VIEW_CHANNEL': false
+        })
+        channel.overwritePermissions(client.user, {
+            'VIEW_CHANNEL': true,
+            'SEND_MESSAGES': true
+        })
+    })
+    
+    await guild.createChannel('Report Log', {type: 'text'}).then(channel => {
+        channel.overwritePermissions(everyoneRole, {
+            'VIEW_CHANNEL': false
+        })
+        channel.overwritePermissions(client.user, {
+            'VIEW_CHANNEL': true,
+            'SEND_MESSAGES': true
+        })
+    })
 })
