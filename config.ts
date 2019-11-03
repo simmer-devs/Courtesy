66909329bbac20a8c7916955bceeba689e78a9bc
Simmer <55367994+simmer-devs@users.noreply.github.com>
2019-11-03 10:43:08 -0700

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,35 @@
+require('dotenv').config({path: '../.env'})
+
+export let config = {
+    "token": process.env.TOKEN,
+    "commands": [
+      "test",
+      "settings",
+      "kick",
+      "ban",
+      "hackban",
+      "tempban",
+      "report",
+      "help"
+    ],
+    "defaultSettings": {
+      prefix: '!',
+      spamFilter: '10',
+      welcomeChannel: 'welcome',
+      welcomeMsg: 'Welcome {{user}} to {{guild}}!',
+      modRole: 'Moderator',
+      adminRole: 'Administrator',
+      reports: []
+    },
+    "memberDefaultSettings": {
+      spamming: []
+    },
+    "reportDefaultSettings": {
+      Title:'',
+      Description: ''
+    },
+    "badWords": [
+      'nigger',
+      'faggot'
+    ] 
+}
