626d9779c9dcf7d35ee165151c28f1815c2c36ce
Simmer <kelly.matthew1218@gmail.com>
2019-11-11 11:10:31 -0700

    fixed spam handler to start timer on first message

--- Handlers/spamHandler.ts
+++ Handlers/spamHandler.ts
@@ -4,7 +4,21 @@ const mongoose = require('mongoose')
 
 export const handleSpam = async (message: Discord.Message, guild: Discord.Guild, memberSettings: any, guildSettings: any) => {
     mongoose.set('useFindAndModify', false) 
-    if(memberSettings.spamming.length >= guildSettings.spamFilter){
+    if(memberSettings.spamming.length === 0){
+        setTimeout(async () => {
+            const memberUpdate = {
+                spamming: []
+            }
+
+            await Member.findOneAndUpdate(
+                {userID: message.member.user.id, guildID: guild.id},
+                memberUpdate
+            )
+
+        }, 10000)
+    }
+    
+    else if(memberSettings.spamming.length >= guildSettings.spamFilter){
         //log spamming/ban instance to moderation channel
         message.delete()
         message.member.ban({days: 7, reason: 'Spamming.'}).catch(err => console.log(err))
@@ -21,7 +35,7 @@ export const handleSpam = async (message: Discord.Message, guild: Discord.Guild,
             {userID: message.member.user.id, guildID: guild.id},
             memberNewUpdate
         )
-    }catch(err){
+    } catch(err){
         console.log(err)
-    }
+    }  
 }
